cd548d22ff3be52d4e130c5267f9069394504464
Albaddev <alioubkeita95@gmail.com>
2022-10-20 16:30:15 +0000

    Update: add get http verb to test in postman

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,29 @@
-console.log('Hey! who are you ?')
-console.log(' step ...')
+const express = require('express')
+const dotenv = require('dotenv')
+const port = process.env.PORT || 5000
+
+
+const app = express()
+const projects = [
+    {
+        name: 'Pokedex',
+        id: 1
+    },
+    {
+        name: 'Portfolio v1',
+        id: 2
+    },
+    {
+        name: 'Github_Viewer',
+        id: 3
+    },
+]
+app.get('/api/projects', (req, res) => {
+    res.status(200).json(projects)
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server running sucessful on port ${port}...`)
+})
